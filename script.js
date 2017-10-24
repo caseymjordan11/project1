8d85880fff8fe95930a60686703b69e6c48e0418
Casey Jordan <Jordan_Casey.bah.com>
2017-10-24 12:27:11 -0400

    colors randomly assign and appear on button click

--- script.js
+++ script.js
@@ -5,9 +5,72 @@ const blueBox = $('.game #blue')
 const greenBox = $('.game #green')
 const yellowBox = $('.game #yellow')
 
+var order = []
+function random(){
+  for (i=0; i<4; i++) {
+  var num = Math.floor(Math.random()*4)
+  order.push(num)
+  }
+}
+
+var flash = {
+  redFlash: function() {
+    redLoad()
+    setTimeout(resetRed, 600)
+  },
+  blueFlash: function(){
+    blueLoad()
+    setTimeout(resetBlue, 600)
+  },
+  greenFlash: function() {
+    greenLoad()
+    setTimeout(resetGreen, 600)
+  },
+  yellowFlash: function() {
+    yellowLoad()
+    setTimeout(resetYellow, 600)
+  }
+}
+
+var keys = Object.keys(flash)
+
 function startLevel () {
+  random()
+  for (i=0; i<4; i++){
+    f = keys[order[i]]
+    flash[f]();
+  }
+  order = []
+}
+
+
+
+
+
+function redLoad(){
   redBox.css('background', 'red')
+}
+function resetRed(){
+  redBox.css('background', 'white')
+}
+
+function blueLoad(){
   blueBox.css('background', 'blue')
+}
+function resetBlue(){
+  blueBox.css('background', 'white')
+}
+
+function yellowLoad(){
   yellowBox.css('background', 'yellow')
+}
+function resetYellow(){
+  yellowBox.css('background', 'white')
+}
+
+function greenLoad(){
   greenBox.css('background', 'green')
+}
+function resetGreen(){
+  greenBox.css('background', 'white')
 }
